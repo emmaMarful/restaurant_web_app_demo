5a986700fda4432dcad7bbf419080d6a0f431d3c
emmaMarful <eamarful@gmail.com>
2023-12-22 12:38:19 +0000

    bootstrap and dynamic link changes

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.MenuList.as_view(), name='home'),
-    path('item/<int:pk>', views.MenuItemDetail.as_view(), name='menu_item')
+    path('item/<int:pk>', views.MenuItemDetail.as_view(), name='menu_item'),
+    path('about/', views.about, name='about')
 ]
 
--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -16,3 +16,8 @@ class MenuList(generic.ListView):
 class MenuItemDetail(generic.DetailView):
     model = Item
     template_name = "menu_item_detail.html"
+
+
+def about(request):
+    return render(request, "about.html")
+
